0b645475df612eb5b27a3e0c33953de037452ae5
William Cardoso <williancard123@gmail.com>
2021-08-25 13:35:15 -0400

    first commit

---
+++ src/routes/middlewares/upload_thumnail.routes.ts
@@ -0,0 +1,25 @@
+import multer from "multer";
+import { uuid } from "uuidv4";
+
+export const multer_define = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './public/thumbnails' )
+    },
+    filename: (req, file, cb) => {
+      const code_for_uniq_img = uuid() +'---'+new Date().toLocaleDateString().replace(/[/]/g, '-') + req.body.title_categorie
+      const new_name = code_for_uniq_img + '-' +file.originalname.split(' ').join('_')
+      cb(null, new_name)
+    }
+  }),
+  fileFilter: (req, file, cb)=>{
+    const extension_img = ['image/png', 'image/jpg', 'image/jpeg','image/gif'].find(
+      (format_accept =>  format_accept === file.mimetype)
+    )
+    if(extension_img) {
+      cb(null, true)
+    } else {
+      cb(null, false)
+    }
+  }
+})
